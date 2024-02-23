66ed20e3691166d50d483a66cd696ab380ef8e11
Adam Ben jeddou <mcadambenjeddou@gmail.com>
2024-02-23 17:47:20 +0100

    .

--- Simulatore Pianeti/Form1.cs
+++ Simulatore Pianeti/Form1.cs
@@ -16,5 +16,10 @@ namespace Simulatore_Pianeti
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
     }
 }
