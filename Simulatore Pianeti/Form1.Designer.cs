f4523133b06cddfe64118899133282e2566e96ed
Fiore <kacuapjfiore@gmail.com>
2024-03-06 19:07:47 +0100

    .

--- Simulatore Pianeti/Form1.Designer.cs
+++ Simulatore Pianeti/Form1.Designer.cs
@@ -30,52 +30,115 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Pianeti = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.S = new System.Windows.Forms.TextBox();
+            this.m = new System.Windows.Forms.TextBox();
+            this.V = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // listBox1
+            // Pianeti
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(9, 10);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(91, 69);
-            this.listBox1.TabIndex = 1;
+            this.Pianeti.FormattingEnabled = true;
+            this.Pianeti.ItemHeight = 16;
+            this.Pianeti.Location = new System.Drawing.Point(12, 12);
+            this.Pianeti.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Pianeti.Name = "Pianeti";
+            this.Pianeti.Size = new System.Drawing.Size(120, 84);
+            this.Pianeti.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 56);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(164, 173);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.Size = new System.Drawing.Size(84, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(173, 102);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(173, 131);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 3;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // S
+            // 
+            this.S.Location = new System.Drawing.Point(164, -3);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(100, 22);
+            this.S.TabIndex = 4;
+            this.S.Text = "S";
+            this.S.TextChanged += new System.EventHandler(this.S_TextChanged);
+            // 
+            // m
+            // 
+            this.m.Location = new System.Drawing.Point(173, 64);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(100, 22);
+            this.m.TabIndex = 5;
+            this.m.Text = "m";
+            // 
+            // V
+            // 
+            this.V.Location = new System.Drawing.Point(173, 25);
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(100, 22);
+            this.V.TabIndex = 6;
+            this.V.Text = "V";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.V);
+            this.Controls.Add(this.m);
+            this.Controls.Add(this.S);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Pianeti);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox Pianeti;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.TextBox S;
+        private System.Windows.Forms.TextBox m;
+        private System.Windows.Forms.TextBox V;
     }
 }
 
--- Simulatore Pianeti/Form1.cs
+++ Simulatore Pianeti/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Vettori;
 
 namespace Simulatore_Pianeti
 {
@@ -23,6 +24,21 @@ namespace Simulatore_Pianeti
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            Pianeti.Items.Add(new Pianeta(double.Parse(m.Text),Vettore.Parse(S.Text),Vettore.Parse(V.Text)));
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            Pianeti.Items.Remove(Pianeti.SelectedItem);
+        }
+
+        private void S_TextChanged(object sender, EventArgs e)
         {
 
         }
