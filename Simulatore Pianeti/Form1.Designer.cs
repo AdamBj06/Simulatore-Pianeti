7ebc0d9149d5287aaa338b3ab1d2b7d8e9522345
CarolinGrigor <125268417+CarolinGrigor@users.noreply.github.com>
2024-03-07 08:02:46 +0100

    .

--- Simulatore Pianeti/Form1.Designer.cs
+++ Simulatore Pianeti/Form1.Designer.cs
@@ -31,12 +31,20 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.Pianeti = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
             this.Add = new System.Windows.Forms.Button();
             this.Remove = new System.Windows.Forms.Button();
-            this.S = new System.Windows.Forms.TextBox();
-            this.m = new System.Windows.Forms.TextBox();
-            this.V = new System.Windows.Forms.TextBox();
+            this.vPos = new System.Windows.Forms.TextBox();
+            this.Massa = new System.Windows.Forms.TextBox();
+            this.vVel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Raggio = new System.Windows.Forms.TextBox();
+            this.Colori = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // timer1
@@ -46,82 +54,166 @@
             // Pianeti
             // 
             this.Pianeti.FormattingEnabled = true;
-            this.Pianeti.ItemHeight = 16;
-            this.Pianeti.Location = new System.Drawing.Point(12, 12);
-            this.Pianeti.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Pianeti.Location = new System.Drawing.Point(9, 11);
+            this.Pianeti.Margin = new System.Windows.Forms.Padding(2);
             this.Pianeti.Name = "Pianeti";
-            this.Pianeti.Size = new System.Drawing.Size(120, 84);
+            this.Pianeti.Size = new System.Drawing.Size(255, 381);
             this.Pianeti.TabIndex = 1;
             // 
-            // button1
+            // Play
             // 
-            this.button1.Location = new System.Drawing.Point(164, 173);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Play";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Play.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Play.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play.Location = new System.Drawing.Point(615, 153);
+            this.Play.Margin = new System.Windows.Forms.Padding(2);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(68, 68);
+            this.Play.TabIndex = 0;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
             // 
             // Add
             // 
-            this.Add.Location = new System.Drawing.Point(173, 102);
+            this.Add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(459, 153);
+            this.Add.Margin = new System.Windows.Forms.Padding(2);
             this.Add.Name = "Add";
-            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.Size = new System.Drawing.Size(68, 68);
             this.Add.TabIndex = 2;
             this.Add.Text = "Add";
-            this.Add.UseVisualStyleBackColor = true;
+            this.Add.UseVisualStyleBackColor = false;
             this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
             // Remove
             // 
-            this.Remove.Location = new System.Drawing.Point(173, 131);
+            this.Remove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remove.Location = new System.Drawing.Point(531, 153);
+            this.Remove.Margin = new System.Windows.Forms.Padding(2);
             this.Remove.Name = "Remove";
-            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.Size = new System.Drawing.Size(80, 68);
             this.Remove.TabIndex = 3;
             this.Remove.Text = "Remove";
-            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.UseVisualStyleBackColor = false;
             this.Remove.Click += new System.EventHandler(this.Remove_Click);
             // 
-            // S
+            // vPos
             // 
-            this.S.Location = new System.Drawing.Point(164, -3);
-            this.S.Name = "S";
-            this.S.Size = new System.Drawing.Size(100, 22);
-            this.S.TabIndex = 4;
-            this.S.Text = "S";
-            this.S.TextChanged += new System.EventHandler(this.S_TextChanged);
+            this.vPos.Location = new System.Drawing.Point(552, 59);
+            this.vPos.Margin = new System.Windows.Forms.Padding(2);
+            this.vPos.Name = "vPos";
+            this.vPos.Size = new System.Drawing.Size(131, 20);
+            this.vPos.TabIndex = 4;
             // 
-            // m
+            // Massa
             // 
-            this.m.Location = new System.Drawing.Point(173, 64);
-            this.m.Name = "m";
-            this.m.Size = new System.Drawing.Size(100, 22);
-            this.m.TabIndex = 5;
-            this.m.Text = "m";
+            this.Massa.Location = new System.Drawing.Point(552, 107);
+            this.Massa.Margin = new System.Windows.Forms.Padding(2);
+            this.Massa.Name = "Massa";
+            this.Massa.Size = new System.Drawing.Size(131, 20);
+            this.Massa.TabIndex = 5;
             // 
-            // V
+            // vVel
             // 
-            this.V.Location = new System.Drawing.Point(173, 25);
-            this.V.Name = "V";
-            this.V.Size = new System.Drawing.Size(100, 22);
-            this.V.TabIndex = 6;
-            this.V.Text = "V";
+            this.vVel.Location = new System.Drawing.Point(552, 83);
+            this.vVel.Margin = new System.Windows.Forms.Padding(2);
+            this.vVel.Name = "vVel";
+            this.vVel.Size = new System.Drawing.Size(131, 20);
+            this.vVel.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(456, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Posizione (x,y)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Velocità (x,y)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Massa (kg)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(456, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Nome";
+            // 
+            // Nome
+            // 
+            this.Nome.Location = new System.Drawing.Point(552, 11);
+            this.Nome.Margin = new System.Windows.Forms.Padding(2);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(131, 20);
+            this.Nome.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Raggio";
+            // 
+            // Raggio
+            // 
+            this.Raggio.Location = new System.Drawing.Point(552, 35);
+            this.Raggio.Margin = new System.Windows.Forms.Padding(2);
+            this.Raggio.Name = "Raggio";
+            this.Raggio.Size = new System.Drawing.Size(131, 20);
+            this.Raggio.TabIndex = 12;
+            // 
+            // Colori
+            // 
+            this.Colori.AllowDrop = true;
+            this.Colori.FormattingEnabled = true;
+            this.Colori.Location = new System.Drawing.Point(296, 18);
+            this.Colori.Name = "Colori";
+            this.Colori.Size = new System.Drawing.Size(121, 21);
+            this.Colori.TabIndex = 14;
+            this.Colori.SelectedIndexChanged += new System.EventHandler(this.Colori_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.V);
-            this.Controls.Add(this.m);
-            this.Controls.Add(this.S);
+            this.ClientSize = new System.Drawing.Size(694, 407);
+            this.Controls.Add(this.Colori);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Raggio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vVel);
+            this.Controls.Add(this.Massa);
+            this.Controls.Add(this.vPos);
             this.Controls.Add(this.Remove);
             this.Controls.Add(this.Add);
             this.Controls.Add(this.Pianeti);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.Play);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -133,12 +225,20 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ListBox Pianeti;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Play;
         private System.Windows.Forms.Button Add;
         private System.Windows.Forms.Button Remove;
-        private System.Windows.Forms.TextBox S;
-        private System.Windows.Forms.TextBox m;
-        private System.Windows.Forms.TextBox V;
+        private System.Windows.Forms.TextBox vPos;
+        private System.Windows.Forms.TextBox Massa;
+        private System.Windows.Forms.TextBox vVel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Nome;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Raggio;
+        private System.Windows.Forms.ComboBox Colori;
     }
 }
 
--- Simulatore Pianeti/Form1.cs
+++ Simulatore Pianeti/Form1.cs
@@ -8,14 +8,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vettori;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Simulatore_Pianeti
 {
     public partial class Form1 : Form
     {
+        private List<Pianeta> listPianeti = new List<Pianeta>();
         public Form1()
         {
             InitializeComponent();
+            InitializeColorsComboBox();
+        }
+
+        private void InitializeColorsComboBox()
+        {
+            Colori.Text = "Scegli un colore";
+
+            foreach (KnownColor knownColor in Enum.GetValues(typeof(KnownColor)))
+            {
+                Color color = Color.FromKnownColor(knownColor);
+                if (!color.IsSystemColor)
+                {
+                    Colori.Items.Add(color.Name);
+                }
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -23,22 +40,30 @@ namespace Simulatore_Pianeti
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void Add_Click(object sender, EventArgs e)
         {
-            Pianeti.Items.Add(new Pianeta(double.Parse(m.Text),Vettore.Parse(S.Text),Vettore.Parse(V.Text)));
+            listPianeti.Add(new Pianeta((Color)Colori.SelectedItem, double.Parse(Raggio.Text), double.Parse(Massa.Text), Vettore.Parse(vPos.Text), Vettore.Parse(vVel.Text)));
+            Pianeti.Items.Add(Nome.Text);
+
+            Nome.Clear();
+            Raggio.Clear();
+            vPos.Clear();
+            vVel.Clear();
+            Massa.Clear();
         }
 
         private void Remove_Click(object sender, EventArgs e)
         {
             Pianeti.Items.Remove(Pianeti.SelectedItem);
+
         }
 
-        private void S_TextChanged(object sender, EventArgs e)
+        private void Colori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color colore = (Color)Colori.SelectedItem;
+        }
+
+        private void Play_Click(object sender, EventArgs e)
         {
 
         }
