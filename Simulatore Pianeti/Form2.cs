428e14f098134370ad818f62fa64a6e76b8bd4cb
Adam Ben jeddou <mcadambenjeddou@gmail.com>
2024-03-14 22:51:13 +0100

    .

--- Simulatore Pianeti/Form2.cs
+++ Simulatore Pianeti/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Security.Policy;
@@ -28,7 +29,7 @@ namespace Simulatore_Pianeti
             cronometro_fps.Start();
         }
 
-        public Pianeta pianeta;
+        public Pianeta pianetaSelezionato;
         public bool mousePressed;
         public int mouseX, mouseY, mouseX2, mouseY2;
         private void timer1_Tick(object sender, EventArgs e)
@@ -40,9 +41,9 @@ namespace Simulatore_Pianeti
 
             Refresh();
 
-            if (pianeta != null)
+            if (pianetaSelezionato != null)
             {
-                label.Text = InformazioniPianeta(pianeta);
+                label.Text = InformazioniPianeta(pianetaSelezionato);
             }
 
             lbl_fps.Text = (1000 / cronometro_fps.ElapsedMilliseconds).ToString();
@@ -65,6 +66,8 @@ namespace Simulatore_Pianeti
 
         public float zoom = 1.0f;
         public float traslazioneX = 0, traslazioneY = 0;
+        public Matrix transformMatrix;
+        public PointF[] centri;
         private void Form2_Paint(object sender, PaintEventArgs e)
         {
             trackBar_speed.Location = new Point(ClientSize.Width - trackBar_speed.Width, ClientSize.Height - trackBar_speed.Height);
@@ -72,27 +75,72 @@ namespace Simulatore_Pianeti
             lbl_fps.Location = new Point(ClientSize.Width - lbl_fps.Width - 20, 10);
 
             Graphics g = CreateGraphics();
-            
             g.ScaleTransform(zoom, zoom);
             g.TranslateTransform(traslazioneX, traslazioneY);
+            transformMatrix = g.Transform;
 
-            foreach (Pianeta p in planetario.Pianeti)//disegna tutti i pianeti/stelle
+            centri = new PointF[planetario.Pianeti.Count];
+            for (int i = 0; i < centri.Length; i++)//disegna tutti i pianeti/stelle
             {
-                float x, y;
-                float r = (float)(p.Raggio / 1e7);
+                float xc = (float)Math.Round(planetario.Pianeti[i].Posizione.X / 1e9);
+                float yc = Height - (float)Math.Round(planetario.Pianeti[i].Posizione.Y / 1e9);
+                centri[i] = new PointF(xc, yc);
+                float r = (float)(planetario.Pianeti[i].Raggio / 1e7);
                 if (r > 8)
                 {
-                    x = (float)(Math.Round(p.Posizione.X / 1e9) - (float)(r / 2));
-                    y = Height - (float)(Math.Round(p.Posizione.Y / 1e9) + (float)(r / 2));
-                    g.FillEllipse(new SolidBrush(p.Colore), x, y, r, r);
+                    float x = xc - r / 2;
+                    float y = yc - r / 2;
+                    g.FillEllipse(new SolidBrush(planetario.Pianeti[i].Colore), x, y, r, r);
                 }
                 else
                 {
-                    x = (float)Math.Round(p.Posizione.X / 1e9 - 4);
-                    y = Height - (float)Math.Round(p.Posizione.Y / 1e9 - 4);
-                    g.FillEllipse(new SolidBrush(p.Colore), x, y, 8, 8);
+                    float x = xc - 4;
+                    float y = yc - 4;
+                    g.FillEllipse(new SolidBrush(planetario.Pianeti[i].Colore), x, y, 8, 8);
                 }
             }
+        }
+
+        private void Form2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.X > trackBar_speed.Location.X && e.Y > trackBar_speed.Location.Y)
+            {
+                trackBar_speed.Enabled = true;
+            }
+            else
+            {
+                trackBar_speed.Enabled = false;
+            }
+
+            transformMatrix.TransformPoints(centri);
+            for (int i = 0; i < centri.Length; i++)
+            {
+                int r = (int)(planetario.Pianeti[i].Raggio / 1e7 * zoom);
+                if (r > 8 && DentroCerchio((int)centri[i].X, (int)centri[i].Y, r, e.X, e.Y))
+                {
+                    pianetaSelezionato = planetario.Pianeti[i];
+                }
+                else if (DentroCerchio((int)centri[i].X, (int)centri[i].Y, 8, e.X, e.Y))
+                {
+                    pianetaSelezionato = planetario.Pianeti[i];
+                }
+            }
+
+            if (pianetaSelezionato != null)
+            {
+                label.Text = InformazioniPianeta(pianetaSelezionato);
+            }
+        }
+
+        public static bool DentroCerchio(int xc, int yc, int r, int x, int y)//controlla se la posizione (x;y) è dentro il cerchio
+        {
+            return (xc - x) * (xc - x) + (yc - y) * (yc - y) <= r * r;
+        }
+
+        public string InformazioniPianeta(Pianeta p)
+        {//(per adam) guardare Iformattable
+            return string.Format("Pianeta: {0}\nMassa: {1}\nPosizione: {2}\nVelocità: {3}\nRaggio: {4}",
+                                 p.Colore, p.Massa, p.Posizione.ToString("0.0000E0"), p.Velocità.ToString("0.0000E0"), p.Raggio.ToString("0.0000E0"));
         }
 
         private void Form2_KeyDown(object sender, KeyEventArgs e)
@@ -165,14 +213,14 @@ namespace Simulatore_Pianeti
             scr2obj(out mx0, out my0, e.X, e.Y);
             if (e.Delta < 0)
             {
-                zoom -= 0.1f;
+                zoom -= 0.05f;
                 obj2scr(out mx0, out my0, mx0, my0);
                 traslazioneX += (e.X - mx0) / zoom;
                 traslazioneY += (e.Y - my0) / zoom;
             }
             else
             {
-                zoom += 0.1f;
+                zoom += 0.05f;
                 obj2scr(out mx0, out my0, mx0, my0);
                 traslazioneX += (e.X - mx0) / zoom;
                 traslazioneY += (e.Y - my0) / zoom;
@@ -194,49 +242,6 @@ namespace Simulatore_Pianeti
         {
             planetario.DeltaT = trackBar_speed.Value;
             lbl_speed.Text = (planetario.DeltaT * 1.66666).ToString(".00") + "g/s; " + (planetario.DeltaT).ToString(".00") + "h/tick";
-        }
-
-        private void Form2_MouseClick(object sender, MouseEventArgs e)
-        {
-            if(e.X > trackBar_speed.Location.X && e.Y > trackBar_speed.Location.Y) 
-            { 
-                trackBar_speed.Enabled = true;
-            }
-            else
-            {
-                trackBar_speed.Enabled = false;
-            }
-
-            foreach (Pianeta p in planetario.Pianeti)//non funziona se cambi lo zoom...
-            {
-                int xc = (int)(Math.Round(p.Posizione.X / 1e9) * zoom - traslazioneX);//centro del cerchio/pianeta
-                int yc = Height - (int)(Math.Round(p.Posizione.Y / 1e9) * zoom + traslazioneY);//centro del cerchio/pianeta
-                int r = (int)(p.Raggio / 1e7 * zoom);//raggio del cerchio/pianeta
-                if (r > 8 && DentroCerchio(xc, yc, r, e.X, e.Y))
-                {
-                    pianeta = p;
-                }
-                else if (DentroCerchio(xc, yc, 8, e.X, e.Y))
-                {
-                    pianeta = p;
-                }
-            }
-
-            if (pianeta != null)
-            {
-                label.Text = InformazioniPianeta(pianeta);
-            }
-        }
-
-        public static bool DentroCerchio(int xc, int yc, int r, int x, int y)//controlla se la posizione (x;y) è dentro il cerchio
-        {
-            return (xc - x) * (xc - x) + (yc - y) * (yc - y) <= r * r;
-        }
-
-        public string InformazioniPianeta(Pianeta p)
-        {//(per adam) guardare Iformattable
-            return string.Format("Pianeta: {0}\nMassa: {1}\nPosizione: {2}\nVelocità: {3}\nRaggio: {4}",
-                                 p.Colore, p.Massa, p.Posizione.ToString("0.0000E0"), p.Velocità.ToString("0.0000E0"), p.Raggio.ToString("0.0000E0"));
         }
     }
 }
